c507a6ae5c9c4de2d33d6782d1bdb7ea07ead809
oculosdanilo <oculosdanilo@gmail.com>
2023-05-26 15:29:47 -0300

    tela mais

---
+++ lib/home/mais/mais.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mais extends StatefulWidget {
+  const Mais({super.key});
+
+  @override
+  State<Mais> createState() => _MaisState();
+}
+
+class _MaisState extends State<Mais> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
