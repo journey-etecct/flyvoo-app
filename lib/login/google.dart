2da38b7e0c89ade9d3fa5e4742e4f80a757df384
oculosdanilo <oculosdanilo@gmail.com>
2023-06-03 13:12:38 -0300

    06/03/2023 13:12:38

---
+++ lib/login/google.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Google extends StatefulWidget {
+  const Google({super.key});
+
+  @override
+  State<Google> createState() => _GoogleState();
+}
+
+class _GoogleState extends State<Google> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
