011c15c8971d0158627a893d697848f66aee1681
oculosdanilo <oculosdanilo@gmail.com>
2023-06-20 22:47:18 -0300

    06/20/2023 22:47:18

---
+++ lib/cadastro/cadastro.dart
@@ -0,0 +1,219 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flyvoo/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:material_symbols_icons/symbols.dart';
+/* import 'package:video_player/video_player.dart'; */ // TODO: video 1
+
+final txtSenha = TextEditingController();
+final txtSenhaConf = TextEditingController();
+
+List<BotaoIndex> _botoes = [
+  BotaoIndex(
+    (null, Symbols.mail_outline_rounded),
+    "Continuar com email",
+  ),
+  BotaoIndex(
+    (AssetImage("assets/icons/email.png"), null),
+    "Continuar com Google",
+  ),
+  BotaoIndex(
+    (AssetImage("assets/icons/user.png"), null),
+    "Continuar com Microsoft",
+  ),
+];
+
+class BotaoIndex {
+  final String text;
+  final (AssetImage?, IconData?) icon;
+  BotaoIndex(this.icon, this.text);
+}
+
+class Cadastro extends StatefulWidget {
+  const Cadastro({Key? key}) : super(key: key);
+
+  @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  /* late VideoPlayerController _controllerLight;
+  late VideoPlayerController _controllerDark;
+
+  void startBothPlayers() async {
+    await _controllerLight.play();
+    await _controllerDark.play();
+  }
+
+  @override
+  void initState() {
+    _controllerLight = VideoPlayerController.asset(
+      "assets/background/light.webm",
+      videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+    )..initialize().then(
+        (_) {
+          _controllerLight.setLooping(true);
+          setState(() {});
+        },
+      );
+    _controllerDark = VideoPlayerController.asset(
+      "assets/background/dark.webm",
+      videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+    )..initialize().then(
+        (_) {
+          _controllerDark.setLooping(true);
+          setState(() {});
+        },
+      );
+    startBothPlayers();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controllerDark.dispose();
+    _controllerLight.dispose();
+    super.dispose();
+  } */ // TODO: video 2
+
+  @override
+  Widget build(BuildContext context) {
+    setState(() {
+      _botoes = [
+        BotaoIndex(
+          (null, Symbols.mail_outline_rounded),
+          "Continuar com email",
+        ),
+        BotaoIndex(
+          (AssetImage("assets/icons/google.png"), null),
+          "Continuar com Google",
+        ),
+        BotaoIndex(
+          (AssetImage("assets/icons/microsoft.png"), null),
+          "Continuar com Microsoft",
+        ),
+      ];
+    });
+    return Scaffold(
+      body: Stack(
+        children: [
+          /* AnimatedOpacity(
+            opacity: dark ? 1 : 0,
+            duration: const Duration(milliseconds: 300),
+            child: SizedBox.expand(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: _controllerDark.value.size.width,
+                  height: _controllerDark.value.size.height,
+                  child: VideoPlayer(_controllerDark),
+                ),
+              ),
+            ),
+          ), */ // TODO: video 3
+          Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      "Cadastro",
+                      style: TextStyle(
+                        fontFamily: "Queensides Light",
+                        fontSize: 60,
+                      ),
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  itemBuilder: (context, index) => Container(
+                    margin: const EdgeInsets.fromLTRB(60, 0, 60, 10),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: tema["fundo"],
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          blurRadius: 4,
+                          spreadRadius: 0,
+                          offset: const Offset(0, 4),
+                          color: const Color(0xff000000).withOpacity(0.25),
+                        ),
+                      ],
+                    ),
+                    child: CupertinoButton(
+                      borderRadius: BorderRadius.circular(100),
+                      color: tema["botaoIndex"],
+                      padding: const EdgeInsets.fromLTRB(0, 9, 0, 9),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 25,
+                          ),
+                          _botoes[index].icon.$1 != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image(
+                                    image: _botoes[index].icon.$1!,
+                                    height: 29,
+                                    color: tema["textoBotaoIndex"],
+                                  ),
+                                )
+                              : Icon(
+                                  _botoes[index].icon.$2,
+                                  size: 30,
+                                  color: tema["textoBotaoIndex"],
+                                ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            _botoes[index].text,
+                            style: GoogleFonts.inter(
+                              color: tema["textoBotaoIndex"],
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  itemCount: _botoes.length,
+                  shrinkWrap: true,
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Text.rich(
+                  style: GoogleFonts.inter(
+                    color: tema["noFundo"],
+                  ),
+                  TextSpan(
+                    text: "Já possui cadastro? ",
+                    children: <InlineSpan>[
+                      TextSpan(
+                        text: "Clique aqui",
+                        style: GoogleFonts.inter(
+                          color:
+                              dark ? tema["primaria"] : tema["textoBotaoIndex"],
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/cadastro/telas/tela1.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:flyvoo/cadastro/cadastro.dart';
+import 'package:flyvoo/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final _txtNome = TextEditingController();
+final _txtEmail = TextEditingController();
+
+class Tela1 extends StatefulWidget {
+  final Key formKey;
+  const Tela1(this.formKey, {super.key});
+
+  @override
+  State<Tela1> createState() => _Tela1State();
+}
+
+class _Tela1State extends State<Tela1> {
+  IconData _iconeOlho = Icons.visibility_rounded;
+  bool _txtEscondido = true;
+  final _senhaConfKey = GlobalKey<FormFieldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget.formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _txtNome,
+            textInputAction: TextInputAction.next,
+            textCapitalization: TextCapitalization.words,
+            keyboardType: TextInputType.text,
+            maxLength: 255,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "*Obrigatório";
+              } else if (value.contains(
+                RegExp(
+                  r'''[!@#<>?":,.'_/`~;[\]\\|=+)(*&^%0-9-]''',
+                ),
+              )) {
+                return "Nome inválido";
+              } else if (!value.contains(
+                RegExp(
+                  r'^[A-ZÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð][a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]{1,}( {1,2}[A-ZÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð][a-za-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]{1,}){1,10}$',
+                ),
+              )) {
+                return "Insira seu nome completo";
+              }
+              return null;
+            },
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            autofillHints: const [AutofillHints.name],
+            decoration: InputDecoration(
+              labelText: "Nome Completo",
+              labelStyle: GoogleFonts.inter(
+                fontSize: 20,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: tema["primaria"]!,
+                ),
+              ),
+            ),
+            cursorColor: tema["primaria"],
+          ),
+          TextFormField(
+            controller: _txtEmail,
+            textInputAction: TextInputAction.next,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "*Obrigatório";
+              } else if (!value.contains(
+                RegExp(
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+                ),
+              ) /* !value.contains("@gmail.com") &&
+                  !value.contains("@etec.sp.gov.br") &&
+                  !value.contains("@outlook.com") &&
+                  !value.contains("@hotmail.com") &&
+                  !value.contains("@yahoo.com") &&
+                  !value.contains("@terra.com.br") */
+                  ) {
+                return "Email inválido";
+              }
+              return null;
+            },
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            autofillHints: const [AutofillHints.email],
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Email",
+              labelStyle: GoogleFonts.inter(
+                fontSize: 20,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: tema["primaria"]!,
+                ),
+              ),
+            ),
+            cursorColor: tema["primaria"],
+          ),
+          /* TextFormField(
+            controller: _txtTelefone,
+            keyboardType: TextInputType.phone,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "*Obrigatório";
+              } else if (value.length < 14) {
+                return "Muito curto";
+              }
+              return null;
+            },
+            autofillHints: const [AutofillHints.telephoneNumberNational],
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+              labelText: "Telefone Pessoal (Celular)",
+              labelStyle: GoogleFonts.inter(fontSize: 20),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: tema["primaria"]!,
+                ),
+              ),
+            ),
+            cursorColor: tema["primaria"],
+            inputFormatters: [
+              PhoneInputFormatter(
+                defaultCountryCode: "BR",
+              )
+            ],
+          ), */
+          TextFormField(
+            controller: txtSenha,
+            enableInteractiveSelection: false,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "*Obrigatório";
+              } else if (value.length < 8) {
+                return "Sua senha precisa ter mais de 8 caracteres.";
+              }
+              return null;
+            },
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            onChanged: (value) => _senhaConfKey.currentState!.validate(),
+            autofillHints: const [AutofillHints.password],
+            obscureText: _txtEscondido,
+            keyboardType: TextInputType.visiblePassword,
+            decoration: InputDecoration(
+              labelText: "Senha",
+              labelStyle: GoogleFonts.inter(
+                fontSize: 20,
+              ),
+              suffix: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    child: Icon(
+                      _iconeOlho,
+                      size: 20,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        if (_txtEscondido) {
+                          _iconeOlho = Icons.visibility_off_rounded;
+                          _txtEscondido = false;
+                        } else {
+                          _iconeOlho = Icons.visibility_rounded;
+                          _txtEscondido = true;
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: tema["primaria"]!,
+                ),
+              ),
+            ),
+            cursorColor: tema["primaria"],
+          ),
+          TextFormField(
+            key: _senhaConfKey,
+            controller: txtSenhaConf,
+            enableInteractiveSelection: false,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "*Obrigatório";
+              } else if (txtSenha.text != value) {
+                return "Senhas não coincidem";
+              } else {
+                return null;
+              }
+            },
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            autofillHints: const [AutofillHints.password],
+            obscureText: _txtEscondido,
+            keyboardType: TextInputType.visiblePassword,
+            decoration: InputDecoration(
+              labelText: "Confirmação da Senha",
+              labelStyle: GoogleFonts.inter(
+                fontSize: 20,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: tema["primaria"]!,
+                ),
+              ),
+            ),
+            cursorColor: tema["primaria"],
+          ),
+        ],
+      ),
+    );
+  }
+}
