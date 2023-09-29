63fb32adfab129539cefe93fe47aee52182dd592
oculosdanilo <oculosdanilo@gmail.com>
2023-09-29 15:31:15 -0300

    tela empresas finalizada

--- lib/home/empresas/empresas.dart
+++ lib/home/empresas/empresas.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:flyvoo/tema.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:icons_plus/icons_plus.dart';
 
 class Empresas extends StatefulWidget {
   const Empresas({super.key});
@@ -13,8 +10,37 @@ class Empresas extends StatefulWidget {
 }
 
 class _EmpresasState extends State<Empresas> {
+  var list = <Widget>[];
+
   @override
   Widget build(BuildContext context) {
+    list = <Widget>[];
+    for (var i = 0; i < 50; i++) {
+      list.add(
+        const Padding(
+          padding: EdgeInsets.all(17.0),
+          child: CardEmpresas(),
+        ),
+      );
+      i != 49
+          ? list.add(
+              Divider(
+                color: tema["texto"],
+              ),
+            )
+          : list.add(
+              const Padding(
+                padding: EdgeInsets.only(
+                  bottom: 50,
+                  top: 30,
+                ),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+    }
+
     return Column(
       children: [
         Text(
@@ -26,25 +52,96 @@ class _EmpresasState extends State<Empresas> {
             color: tema["texto"],
           ),
         ),
+        const SizedBox(
+          height: 10,
+        ),
         Expanded(
           child: ListView.builder(
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Column(
                 children: [
-                  Expanded(
-                    child: Container(
-                      color: tema["botaoIndex"],
-                      child: Icon(PixelArtIcons.bullseye_arrow),
-                    ),
-                  ),
+                  list[index],
                 ],
-              ),
-            ),
+              );
+            },
             shrinkWrap: true,
-            itemCount: 50,
+            itemCount: list.length,
           ),
         ),
+      ],
+    );
+  }
+}
+
+class CardEmpresas extends StatelessWidget {
+  const CardEmpresas({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipOval(
+          child: Image.asset(
+            "assets/background/loading.gif",
+            width: 50,
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Estágio em Web Design | Home Office",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.inter(
+                  color: tema["texto"],
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                "Google",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.inter(
+                  color: tema["texto"],
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "São Paulo, São Paulo, Brasil(Remoto)",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.inter(
+                  color: tema["texto"],
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Image.asset(
+                    "assets/icons/alvo.png",
+                    color: tema["texto"],
+                  ),
+                  const SizedBox(
+                    width: 7,
+                  ),
+                  const Text("Recrutando"),
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
