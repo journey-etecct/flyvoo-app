6a0abdf6b3dd3014279d1087fe0464dbcb062d0a
oculosdanilo <oculosdanilo@gmail.com>
2023-10-02 00:15:30 -0300

    10/02/2023 00:15:30

--- lib/home/principal/principal.dart
+++ lib/home/principal/principal.dart
@@ -5,9 +5,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flyvoo/tema.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+Areas areaAtual = Areas.logicoMat;
 
 class Principal extends StatefulWidget {
   const Principal({super.key});
@@ -31,18 +34,27 @@ class _PrincipalState extends State<Principal> {
   void initState() {
     super.initState();
     _init();
+    Areas.corporalCin;
   }
 
   @override
   Widget build(BuildContext context) {
     return CarouselSlider.builder(
-      itemCount: 3,
-      itemBuilder: (context, indexTeorico, indexReal) => Especialidade(
-        index: indexReal,
-      ),
+      itemCount: 9,
+      itemBuilder: (context, indexTeorico, indexReal) {
+        return Especialidade(
+          index: indexTeorico,
+        );
+      },
       options: CarouselOptions(
         height: double.infinity,
         enlargeCenterPage: true,
+        onPageChanged: (index, reason) {
+          setState(() {
+            areaAtual = Areas.values[index];
+          });
+          debugPrint(Areas.values[index].toString());
+        },
       ),
     );
   }
@@ -58,22 +70,20 @@ class Especialidade extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          "Lógica",
+          Areas.values[index].nome,
           style: GoogleFonts.inter(
             fontSize: 28,
             fontWeight: FontWeight.w400,
             letterSpacing: -0.41,
           ),
         ),
-        Text(
-          "Nível 1",
-          style: GoogleFonts.inter(
-            fontSize: 24,
-            fontWeight: FontWeight.w400,
-            letterSpacing: -0.41,
-          ),
+        SizedBox(
+          height: 15,
         ),
-        SvgPicture.asset("assets/imagens/logica.svg"),
+        SvgPicture.asset(
+          "assets/imagens/areas/${Areas.values[index].name}.svg",
+          height: 400,
+        ),
         RatingBar(
           onRatingUpdate: (value) {},
           glow: false,
@@ -114,27 +124,97 @@ class Especialidade extends StatelessWidget {
                 blurRadius: 20,
                 spreadRadius: 0,
                 offset: const Offset(0, 3),
-                color: const Color(0xffF81B50).withOpacity(
-                  0.5,
-                ),
+                color: dark
+                    ? Areas.values[index].primaryDark().withOpacity(
+                          0.5,
+                        )
+                    : Areas.values[index].primaryLight().withOpacity(
+                          0.5,
+                        ),
               ),
             ],
           ),
           child: CupertinoButton(
-              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-              color: const Color(0xffF81B50),
-              borderRadius: BorderRadius.circular(50),
-              onPressed: () {},
-              child: Text(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+            color: dark
+                ? Areas.values[index].primaryDark()
+                : Areas.values[index].primaryLight(),
+            borderRadius: BorderRadius.circular(50),
+            onPressed: () {},
+            child: Text(
+              'VER MAIS',
+              style: GoogleFonts.inter(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            /* Text(
                 "INICIAR",
                 style: GoogleFonts.inter(
                   fontWeight: FontWeight.w600,
                   fontSize: 20,
                   color: Colors.white,
                 ),
-              )),
+              ), */
+          ),
         ),
       ],
+    );
+  }
+}
+
+class FadeIndexedStack extends StatefulWidget {
+  final int index;
+  final List<Widget> children;
+  final Duration duration;
+
+  const FadeIndexedStack({
+    Key? key,
+    required this.index,
+    required this.children,
+    this.duration = const Duration(
+      milliseconds: 800,
+    ),
+  }) : super(key: key);
+
+  @override
+  State<FadeIndexedStack> createState() => _FadeIndexedStackState();
+}
+
+class _FadeIndexedStackState extends State<FadeIndexedStack>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void didUpdateWidget(FadeIndexedStack oldWidget) {
+    if (widget.index != oldWidget.index) {
+      _controller.forward(from: 0.0);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _controller,
+      child: IndexedStack(
+        index: widget.index,
+        children: widget.children,
+      ),
     );
   }
 }
--- lib/tema.dart
+++ lib/tema.dart
@@ -44,7 +44,6 @@ enum Tema {
   }
 }
 
-Map<String, List> paletas = {};
 Map<String, Color> tema = {
   "primaria": dark ? const Color(0xff00FFD8) : const Color(0xffFB5607),
   "secundaria": dark ? const Color(0xff31b6b0) : Colors.black,
@@ -65,3 +64,60 @@ Map<String, Color> tema = {
       : const Color(0xffFFD3BD).withOpacity(0.60),
   "textoBotaoIndex": dark ? Colors.white : const Color(0xffA93535),
 };
+
+enum Areas {
+  naturalista("Naturalista"),
+  logicoMat("Logico-Matemática"),
+  existencial("Existencial"),
+  intrapessoal("Intrapessoal"),
+  linguistica("Linguística"),
+  corporalCin("Corporal-cinestética"),
+  interpessoal("Interpessoal"),
+  musical("Musical"),
+  espacial("Espacial");
+
+  const Areas(this.nome);
+  final String nome;
+
+  Color navbar() {
+    return switch (this) {
+      Areas.corporalCin => const Color(0xffFF7D7D),
+      Areas.espacial => const Color(0xff4A8AEC),
+      Areas.existencial => const Color(0xffAD44ED),
+      Areas.interpessoal => const Color(0xffFF6CBC),
+      Areas.intrapessoal => const Color(0xffFFD600),
+      Areas.linguistica => const Color(0xffF5C13B),
+      Areas.logicoMat => const Color(0xff685BFF),
+      Areas.musical => const Color(0xff24D6C1),
+      Areas.naturalista => const Color(0xff39C023),
+    };
+  }
+
+  Color primaryLight() {
+    return switch (this) {
+      Areas.corporalCin => const Color(0xffFF7D7D),
+      Areas.espacial => const Color(0xff367EED),
+      Areas.existencial => const Color(0xff942DD3),
+      Areas.interpessoal => const Color(0xffFF6CBC),
+      Areas.intrapessoal => const Color(0xffefc900),
+      Areas.linguistica => const Color(0xffFFA800),
+      Areas.logicoMat => const Color(0xff3C30CA),
+      Areas.musical => const Color(0xff27BA8E),
+      Areas.naturalista => const Color(0xff31B31C),
+    };
+  }
+
+  Color primaryDark() {
+    return switch (this) {
+      Areas.corporalCin => const Color(0xffFF1879),
+      Areas.espacial => const Color(0xff1866DE),
+      Areas.existencial => const Color(0xff8F3BC2),
+      Areas.interpessoal => const Color(0xffDE328F),
+      Areas.intrapessoal => const Color(0xffFFE500),
+      Areas.linguistica => const Color(0xffFFA800),
+      Areas.logicoMat => const Color(0xff5526B7),
+      Areas.musical => const Color(0xff0CCFB7),
+      Areas.naturalista => const Color(0xff39C023),
+    };
+  }
+}
