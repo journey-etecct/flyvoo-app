a4789caab6ba9c43e28fcee91e64e39299f46444
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-10-20 19:50:36 -0300

    10/20/2023 19:50:36

--- lib/home/principal/principal.dart
+++ lib/home/principal/principal.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:animations/animations.dart';
 import 'package:carousel_slider/carousel_slider.dart';
